90cb55c08ba81ccbed93724b8fd221d365cf624b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 18:55:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,12 @@ class Shoe
    @brand = brand
  end
  
- def cobble(:condition =>"new")
+ def cobble()
    puts "Your shoe is as good as new!"
  end 
+ 
+ def condition=(condition:"new")
+end
  
  end
   
